fd66e992ca0bf9fc7ca34cda180b117b7a306519
dominikwasilewski <dominikwas@outlook.com>
2020-03-09 21:41:31 +0000

    app.component.html * Added a consistent toolbar for the app, each component hopefully could fit under the main toolbar
    app.component.ts
    * The main title of the framework has been changed to'OmegaSys' for now meaning 'Omega System' for 'Project Omega', name can be changed in the future
    
    app.module.ts
    * Imported various modules, mainly material/forms (design uses) and routing for navigation, 2 new components added with comments for organisation
    
    index.html
    * Changed the title to represent the framework's name, 'OmegaSys' for now, background addition
    
    styles.scss
    * Added basic universal styling for consistent design
    
    package.json
    * Added support for cdk (useful for material usage), material (for design neat design implementations) and a mock JSON server api that will be used for mocking JSON data/server relationship locally
    
    package-lock.json
    * Added extra dependencies (see above package.json description please)
    
    customers.component.html/scss/spec.ts/ts
    * Sample customer component added, empty shell for now
    
    dashboard.component.html
    * Dashboard that supports a router link to customer component
    * Style addition with attributes (e.g.: 'rowHeight', 'gutterSize'
    * Added tiles for future navigation to the planned pages, 'Customers' page link works
    
    dashboard.component.scss
    * Light blue styling for tiles
    
    dashboard.component.ts
    * Created a component for the dashboard
    
    dashboard.component.spec.ts
    * Default test - Truthy test
    
    app-routing.module.ts
    * Added a basic routing mechanism for navigation - main dashboard (expected to be shown after authentication page) added, customer component added

--- AdminClient/src/app/app.module.ts
+++ AdminClient/src/app/app.module.ts
@@ -1,14 +1,29 @@
+// Modules imported
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { AppRoutingModule } from './app-routing.module';
 
+
+// Components imported
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CustomersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
