38bfb1e3f288a3d0251430bbced972358e726b91
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-03-15 12:43:38 +0000

    Updated node types and TypeScript types to much latests versions

--- RepClient/src/app/home/home.page.ts
+++ RepClient/src/app/home/home.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  templateUrl: 'home.page.html'
 })
 export class HomePage {
 
