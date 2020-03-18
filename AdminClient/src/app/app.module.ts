d4acefc59c985884764a93fcbdd6895bd2cb6cd6
dominikwasilewski <dominikwas@outlook.com>
2020-03-18 14:12:12 +0000

    Added customer component for CRUD customer management
    Addition of a customer service to ease in requests (mocking JSON data)
    
    Added a sample database of customers for testing the component
    
    Minor edits to load in data + imports in customer component .ts

--- AdminClient/src/app/app.module.ts
+++ AdminClient/src/app/app.module.ts
@@ -2,10 +2,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 // Components imported
 import { AppComponent } from './app.component';
@@ -18,13 +21,17 @@ import { CustomersComponent } from './customers/customers.component';
     DashboardComponent,
     CustomersComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    MatToolbarModule,
-    AppRoutingModule,
-    MatGridListModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        MatToolbarModule,
+        AppRoutingModule,
+        MatGridListModule,
+        HttpClientModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatFormFieldModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- AdminClient/src/app/customers/customers.component.ts
+++ AdminClient/src/app/customers/customers.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer';
+import { Observable, of} from 'rxjs';
+import { CustomersService} from '../customers.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  customers: Customer[];
+
+  constructor(private customerService: CustomersService) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  private getData(): void {
+    this.customerService.getCustomers()
+      .pipe(delay(2000)).subscribe(customers => this.customers = customers);
+  }
+
+  private updateCustomerRecord(id: number, column: string, newEntry: string): void {
+    this.customerService.updateCustomerRecord(id, column, newEntry)
+      .subscribe();
   }
 
 }
