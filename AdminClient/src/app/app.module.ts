2cdc75cffd71ccf74e1b76214fcdb9b78a2fabd9
dominikwasilewski <dominikwas@outlook.com>
2020-03-23 14:32:08 +0000

    Removed missing login component

--- AdminClient/src/app/app.module.ts
+++ AdminClient/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbar } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
