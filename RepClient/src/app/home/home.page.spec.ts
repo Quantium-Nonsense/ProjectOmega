68862658e20b041f08a4c991dbadd1be3c03aafb
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-04-04 14:39:02 +0100

    Ensure mock store is provided for home test cases ( home.page.spec.ts )

--- RepClient/src/app/home/home.page.spec.ts
+++ RepClient/src/app/home/home.page.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { HomePage } from './home.page';
 
@@ -10,7 +11,10 @@ describe('HomePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        provideMockStore()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
