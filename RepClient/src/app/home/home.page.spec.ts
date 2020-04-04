e1ab647219cdf05eb47e52a35ad49161f2037a80
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-04-04 16:19:40 +0100

    Test to ensure home page is displaying loading content correctly when companies empty

--- RepClient/src/app/home/home.page.spec.ts
+++ RepClient/src/app/home/home.page.spec.ts
@@ -1,28 +1,66 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-import { provideMockStore } from '@ngrx/store/testing';
-
+import { By } from '@angular/platform-browser';
+import { IonicModule, IonSpinner } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import * as fromApp from './../reducers/index';
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let mockStore: MockStore<fromApp.AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()],
+      imports: [
+        IonicModule.forRoot()
+      ],
       providers: [
         provideMockStore()
-      ]
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
+    mockStore = TestBed.inject(Store) as MockStore<fromApp.AppState>;
+
+    // Set store state
+    mockStore.setState({
+      auth: undefined,
+      company: undefined,
+      home: {
+        companies: undefined,
+        loading: true
+      }
+    });
+
+    // Ensure store reflects new state
+    mockStore.refreshState();
+    // Force angular to pick up changes
     fixture.detectChanges();
+
   }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Expect component to be created
+  });
+
+  it('should display loading companies', () => {
+    // Current state companies are undefined thus loading elements should be there
+    // Initialize component
+    component.ionViewWillEnter();
+    fixture.detectChanges(); // Check for changes
+    // Ensure loading elements exist
+    const loadingBuffer = fixture.debugElement.query(By.css('#loadingElements'));
+
+    // ensure main content not displaying
+    const mainContent = fixture.debugElement.query(By.css('#mainContent'));
+
+    expect(loadingBuffer).toBeTruthy();
+    expect(mainContent).toBeFalsy();
   });
 });
