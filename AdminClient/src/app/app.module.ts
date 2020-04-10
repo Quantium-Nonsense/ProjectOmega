00907fd8a9cc7bf1cf04803e7364ce4e37ef6247
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-04-10 13:38:55 +0100

    Implemented side nav functionality

--- AdminClient/src/app/app.module.ts
+++ AdminClient/src/app/app.module.ts
@@ -1,16 +1,18 @@
 // Modules imported
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'; // <-- NgModel lives here
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-
-
 // Components imported
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomersComponent } from './customers/customers.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,10 @@ import { CustomersComponent } from './customers/customers.component';
     MatToolbarModule,
     AppRoutingModule,
     MatGridListModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
