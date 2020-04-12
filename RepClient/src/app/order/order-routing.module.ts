ae7b6616e2158d4adf0480cf16c276f614c9c4ef
codefactor-io <support@codefactor.io>
2020-04-12 19:00:16 +0000

    [CodeFactor] Apply fixes

--- RepClient/src/app/order/order-routing.module.ts
+++ RepClient/src/app/order/order-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { OrderPage } from './order.page';
 
@@ -12,6 +12,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class OrderPageRoutingModule {}
