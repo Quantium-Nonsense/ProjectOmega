07733d6c251fe77d47d5ac85b7644e5f2a5ccfa3
codefactor-io <support@codefactor.io>
2020-04-13 18:12:51 +0000

    [CodeFactor] Apply fixes

--- RepClient/src/app/shared/component/header/header.component.ts
+++ RepClient/src/app/shared/component/header/header.component.ts
@@ -3,12 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   @Input() title: string;
-
-  constructor() { }
 
   ngOnInit() {}
 
