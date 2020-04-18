75510629fa90a1635437228fa9aa507c5e971896
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-04-18 16:36:06 +0100

    Created user component added as lazy loading

--- AdminClient/src/app/app-routing.module.ts
+++ AdminClient/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/guads/auth.guard';
 import { CustomersComponent } from './customers/customers.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // default path
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'customers', component: CustomersComponent, canActivate: [AuthGuard]},
+  {path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule), canActivate: [AuthGuard]},
   {path: 'auth', component: AuthComponent},
   // Add additional routes here as needed
 ];
--- AdminClient/src/app/app.module.ts
+++ AdminClient/src/app/app.module.ts
@@ -1,28 +1,27 @@
 // Modules imported
-import { MatCardModule } from '@angular/material/card';
-import { BrowserModule } from '@angular/platform-browser';
-import { JwtModule } from '@auth0/angular-jwt';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'; // <-- NgModel lives here
+import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-
-// Components imported
-import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { CustomersComponent } from './customers/customers.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+// Components imported
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 import { AuthEffects } from './auth/store/auth.effects';
+import { CustomersComponent } from './customers/customers.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { appReducer, metaReducers } from './reducers';
 import { SharedModule } from './shared/shared.module';
 
@@ -32,7 +31,7 @@ export const getToken = () => localStorage.getItem(environment.ACCESS_TOKEN);
   declarations: [
     AppComponent,
     DashboardComponent,
-    CustomersComponent
+    CustomersComponent,
   ],
   bootstrap: [AppComponent],
   imports: [
