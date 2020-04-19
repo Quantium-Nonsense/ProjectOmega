952638b03b19d16e55815478bde56211899bb43c
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2020-04-19 21:25:05 +0100

    Applied guard and fixed auth to be dynamically loaded.

--- AdminClient/src/app/app-routing.module.ts
+++ AdminClient/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/guads/auth.guard';
 import { CustomersComponent } from './customers/customers.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,8 +8,8 @@ export const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // default path
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'customers', component: CustomersComponent, canActivate: [AuthGuard]},
-  {path: 'auth', component: AuthComponent},
-  {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule), canActivate: [AuthGuard]},
   // Add additional routes here as needed
 ];
 
