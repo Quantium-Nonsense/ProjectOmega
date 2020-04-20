075292861aeae8704e67f9c30ec718379de4875b
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2020-04-20 02:27:16 +0100

    Fixed unsubscribing from dialog and relative width (with an absolute maximum though)

--- AdminClient/src/app/products/products.component.ts
+++ AdminClient/src/app/products/products.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { Product, ProductsDataSource } from '../../data/products/products-datasource';
 import { DeleteDialogComponent } from '../shared/delete-dialog/delete-dialog.component';
@@ -15,7 +16,7 @@ import { ProductDetailsDialogComponent } from './product-details-dialog/product-
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements AfterViewInit, OnInit {
+export class ProductsComponent implements AfterViewInit, OnInit, OnDestroy {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<Product>;
@@ -23,6 +24,8 @@ export class ProductsComponent implements AfterViewInit, OnInit {
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['supplier', 'name', 'description', 'price', 'actions'];
+
+  private subscriptions = new Subscription();
 
   constructor(
     private router: Router,
@@ -41,32 +44,41 @@ export class ProductsComponent implements AfterViewInit, OnInit {
     this.table.dataSource = this.dataSource;
   }
 
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
   handleReadDetails(product: Product) {
     const dialogRef = this.dialog.open(ProductDetailsDialogComponent, {
-      width: '500px',
+      maxWidth: '500px',
+      width: '66vw',
       data: {product, title: `Details for ${product.name}`, editable: false}
     });
   }
 
   handleEditDetails(product: Product) {
     const dialogRef = this.dialog.open(ProductDetailsDialogComponent, {
-      width: '500px',
+      maxWidth: '500px',
+      width: '66vw',
       data: {product, title: `Edit ${product.name}`, editable: true}
     });
 
-    dialogRef.afterClosed().subscribe(updatedProduct => {
-      if (updatedProduct && !ProductsDataSource.equalsWithoutId(product, updatedProduct)) {
-        this.dataSource.updateItem(updatedProduct);
-        this.snackBar.open(`${updatedProduct.name} was successfully updated`, 'Close', {duration: 3000});
-      } else {
-        this.snackBar.open(`${updatedProduct.name} was not updated as no changes were saved`, 'Close', {duration: 3000});
-      }
-    });
+    this.subscriptions.add(
+      dialogRef.afterClosed().subscribe(updatedProduct => {
+        if (updatedProduct && !ProductsDataSource.equalsWithoutId(product, updatedProduct)) {
+          this.dataSource.updateItem(updatedProduct);
+          this.snackBar.open(`${updatedProduct.name} was successfully updated`, 'Close', {duration: 3000});
+        } else {
+          this.snackBar.open(`${updatedProduct.name} was not updated as no changes were saved`, 'Close', {duration: 3000});
+        }
+      })
+    );
   }
 
   handleCreate() {
     const dialogRef = this.dialog.open(ProductDetailsDialogComponent, {
-      width: '500px',
+      maxWidth: '500px',
+      width: '66vw',
       data: {
         product: null,
         title: 'Create new product',
@@ -74,30 +86,34 @@ export class ProductsComponent implements AfterViewInit, OnInit {
       }
     });
 
-    dialogRef.afterClosed().subscribe(newProduct => {
-      if (newProduct) {
-        newProduct.id = this.dataSource.getNextId();
-        this.dataSource.addItem(newProduct);
-        this.snackBar.open(`${newProduct.name} was successfully added`, 'Close', {duration: 3000});
-      } else {
-        this.snackBar.open(`${newProduct.name} was not updated as no changes were made`, 'Close', {duration: 3000});
-      }
-    });
+    this.subscriptions.add(
+      dialogRef.afterClosed().subscribe(newProduct => {
+        if (newProduct) {
+          newProduct.id = this.dataSource.getNextId();
+          this.dataSource.addItem(newProduct);
+          this.snackBar.open(`${newProduct.name} was successfully added`, 'Close', {duration: 3000});
+        } else {
+          this.snackBar.open(`${newProduct.name} was not updated as no changes were made`, 'Close', {duration: 3000});
+        }
+      })
+    );
   }
 
   handleDeleteDetails(product: Product) {
     const dialogRef = this.dialog.open(DeleteDialogComponent, {
-      width: '500px',
+      width: '66vw',
       data: product.name,
     });
 
-    dialogRef.afterClosed().subscribe(willDelete => {
-      if (willDelete) {
-        this.dataSource.deleteById(product.id);
-        this.snackBar.open(`${product.name} was successfully deleted`, 'Close', {duration: 3000});
-      } else {
-        this.snackBar.open(`${product.name} was not deleted`, 'Close', {duration: 3000});
-      }
-    });
+    this.subscriptions.add(
+      dialogRef.afterClosed().subscribe(willDelete => {
+        if (willDelete) {
+          this.dataSource.deleteById(product.id);
+          this.snackBar.open(`${product.name} was successfully deleted`, 'Close', {duration: 3000});
+        } else {
+          this.snackBar.open(`${product.name} was not deleted`, 'Close', {duration: 3000});
+        }
+      })
+    );
   }
 }
