5f65e62f97742d54c642ba1c8b76d5749c7fb461
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-15 12:54:48 +0100

    Attemp to point travis at karma.config

--- RepClient/karma.conf.js
+++ RepClient/karma.conf.js
@@ -2,39 +2,44 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-        browserDisconnectTimeout: 100000,
-        browserDisconnectTolerance: 3,
-        browserNoActivityTimeout: 600000,
-      }
-    },
-    singleRun: false
-  });
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine', '@angular-devkit/build-angular'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('@angular-devkit/build-angular/plugins/karma')
+		],
+		client: {
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
+		},
+		coverageIstanbulReporter: {
+			dir: require('path').join(__dirname, '../coverage'),
+			reports: ['html', 'lcovonly', 'text-summary'],
+			fixWebpackSourcePaths: true
+		},
+		reporters: ['progress', 'kjhtml'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['Chrome'],
+		customLaunchers: {
+			ChromeHeadlessCI: {
+				base: 'ChromeHeadless',
+				flags: [
+					'--no-sandbox',
+					'--no-watch',
+					'--no-progress',
+					'--code-coverage'
+				],
+				browserDisconnectTimeout: 100000,
+				browserDisconnectTolerance: 3,
+				browserNoActivityTimeout: 600000
+			}
+		},
+		singleRun: false
+	});
 };
