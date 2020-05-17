b1fb19851880196282098591210dd4af084d599c
Xander9 <alex.karp475@gmail.com>
2020-05-17 23:25:30 +0100

    Changed response status to 400 Bad Request

--- Backend/src/main/java/com/project/omega/exceptions/ClientNotFoundException.java
+++ Backend/src/main/java/com/project/omega/exceptions/ClientNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public class ClientNotFoundException extends Exception {
 
         private static final long serialVersionUID = 1L;
