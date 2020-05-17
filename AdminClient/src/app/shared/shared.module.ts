bd502b4d8b0bb3beec5adb141253ad2fd2159d51
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-17 19:24:22 +0100

    Removed async pipe from Shared module

--- AdminClient/src/app/shared/shared.module.ts
+++ AdminClient/src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { TextFieldModule } from '@angular/cdk/text-field';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
