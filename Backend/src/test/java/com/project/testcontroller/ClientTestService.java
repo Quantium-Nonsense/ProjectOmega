2b2dbdb3075d9030705d821bd2a2e862c533feeb
Kaylesh <56653205+Kaylesh@users.noreply.github.com>
2020-05-18 19:17:32 +0100

    test services get client by id

--- Backend/src/test/java/com/project/testcontroller/ClientTestService.java
+++ Backend/src/test/java/com/project/testcontroller/ClientTestService.java
@@ -5,6 +5,7 @@ import com.project.omega.bean.dao.entity.Client;
 import com.project.omega.bean.dao.entity.Product;
 import com.project.omega.exceptions.ClientNotFoundException;
 import com.project.omega.exceptions.NoRecordsFoundException;
+import com.project.omega.exceptions.ProductNotFoundException;
 import com.project.omega.repository.ClientRepository;
 import com.project.omega.service.implmentations.ClientServiceImpl;
 import com.project.omega.service.interfaces.ClientService;
@@ -20,6 +21,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -95,12 +97,38 @@ public class ClientTestService extends OmegaApplicationTests {
 
     @Test
     public void getClientByIdTest_Positive() throws ClientNotFoundException {
-
+        Client client_one = new Client.ClientBuilder()
+                .setId(1L)
+                .setFirst_name("John")
+                .setLast_name("Doe")
+                .setDescription("Buys lots of things")
+                .setCompanyName("Lloyds")
+                .setEmail("a@a.com")
+                .setContactNumber("02088273729")
+                .setNotes("that's a random phone number")
+                .build();
+        Client client_two = new Client.ClientBuilder()
+                .setId(2L)
+                .setFirst_name("Jane")
+                .setLast_name("Smith")
+                .setDescription("Doesn't buy much")
+                .setCompanyName("Sdyoll")
+                .setEmail("b@b.com")
+                .setContactNumber("02088747643")
+                .setNotes("that's another random phone number")
+                .build();
+        Mockito.when(clientRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(client_one));
+        Assert.assertEquals(client_one.getFirst_name(), clientService.getClientById(1L).getFirst_name());
     }
 
     @Test
     public void getClientByIdTest_Negative() throws ClientNotFoundException {
-
+        Mockito.when(clientRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+        try {
+            clientService.getClientById(1L);
+        } catch (ClientNotFoundException e) {
+            Assert.assertTrue(e.getMessage().equals(messages.getMessage("message.clientNotFound", null, null)));
+        }
     }
 
     @Test
