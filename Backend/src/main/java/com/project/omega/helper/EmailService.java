5f7f0f8078948dc6bacb4fa2e50d1c7fba515499
Xander9 <alex.karp475@gmail.com>
2020-05-18 15:52:25 +0100

    Added email sender class.

---
+++ Backend/src/main/java/com/project/omega/helper/EmailService.java
@@ -0,0 +1,37 @@
+package com.project.omega.helper;
+
+import com.sendgrid.*;
+import com.sendgrid.Response;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+
+public class EmailService {
+    @Value("${sendGrid.key}")
+    private static String key;
+
+    public static void send(String from, String to, String subject, String content) throws IOException {
+        Email fromUser = new Email(from);
+        Email toUser = new Email(to);
+        Content contents = new Content("text/plain", content);
+        Mail mail = new Mail(fromUser, subject, toUser, contents);
+
+        SendGrid sg = new SendGrid(System.getenv(key));
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+}
