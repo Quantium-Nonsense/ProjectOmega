656a2d0345e65348120734d606ea2c1bca294533
Kaylesh <56653205+Kaylesh@users.noreply.github.com>
2020-05-18 17:47:19 +0100

    client test service skeleton

--- Backend/src/test/java/com/project/testcontroller/ClientTestService.java
+++ Backend/src/test/java/com/project/testcontroller/ClientTestService.java
@@ -1,9 +1,12 @@
 package com.project.testcontroller;
 
 import com.project.OmegaApplicationTests;
+import com.project.omega.exceptions.ClientNotFoundException;
+import com.project.omega.exceptions.NoRecordsFoundException;
 import com.project.omega.repository.ClientRepository;
 import com.project.omega.service.implmentations.ClientServiceImpl;
 import com.project.omega.service.interfaces.ClientService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,4 +21,41 @@ public class ClientTestService extends OmegaApplicationTests {
 
     @InjectMocks
     ClientService clientService = new ClientServiceImpl();
+
+    @Test
+    public void createClientTest() throws NoRecordsFoundException {
+
+    }
+
+
+    @Test
+    public void getAllClientsTest_Positive() throws NoRecordsFoundException {
+
+    }
+
+    @Test
+    public void getAllClientsTest_Negative() throws NoRecordsFoundException {
+
+    }
+
+    @Test
+    public void getClientByIdTest_Positive() throws ClientNotFoundException {
+
+    }
+
+    @Test
+    public void getClientByIdTest_Negative() throws ClientNotFoundException {
+
+    }
+
+    @Test
+    public void updateClientByIdTest() throws ClientNotFoundException {
+
+    }
+
+    @Test
+    public void deleteClientByIdTest() throws ClientNotFoundException {
+        
+    }
+
 }
