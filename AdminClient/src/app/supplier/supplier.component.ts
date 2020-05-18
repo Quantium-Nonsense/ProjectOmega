b8d4b090a3d4a1e2217a250dd2715b66126bca27
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-18 16:58:20 +0100

    Formatting

--- AdminClient/src/app/supplier/supplier.component.ts
+++ AdminClient/src/app/supplier/supplier.component.ts
@@ -1,65 +1,65 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as fromApp from '../reducers/index';
-import {Store} from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import * as ClientActions from './store/suppliers.actions';
-import {Observable, Subscription} from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import * as fromSuppliers from './store/suppliers.reducer';
-import {MatTableDataSource} from '@angular/material/table';
-import {SupplierModel} from '../shared/model/supplier/supplier.model';
-import {MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { SupplierModel } from '../shared/model/supplier/supplier.model';
+import { MatPaginator } from '@angular/material/paginator';
 import * as SupplierActions from './store/suppliers.actions';
 
 @Component({
-	selector: 'app-clients',
-	templateUrl: './suppliers.component.html',
-	styleUrls: ['./supplier.component.scss']
+  selector: 'app-clients',
+  templateUrl: './suppliers.component.html',
+  styleUrls: ['./supplier.component.scss']
 })
 export class SupplierComponent implements OnInit, OnDestroy {
-	@ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-	isLoading: Observable<boolean>;
-	suppliers: MatTableDataSource<SupplierModel>;
-	displayCols: string[] = ['companyName', 'contactNumber', 'email', 'firstName', 'lastName', 'actions'];
+  isLoading: Observable<boolean>;
+  suppliers: MatTableDataSource<SupplierModel>;
+  displayCols: string[] = ['companyName', 'contactNumber', 'email', 'firstName', 'lastName', 'actions'];
 
-	private sub: Subscription;
+  private sub: Subscription;
 
-	constructor(
-		private store$: Store<fromApp.State>
-	) {
-		this.suppliers = new MatTableDataSource<SupplierModel>([]);
-		this.sub = new Subscription();
-	}
+  constructor(
+      private store$: Store<fromApp.State>
+  ) {
+    this.suppliers = new MatTableDataSource<SupplierModel>([]);
+    this.sub = new Subscription();
+  }
 
-	ngOnInit(): void {
-		// Load all supplier
-		this.store$.dispatch(ClientActions.beginLoadingSuppliers());
-		this.isLoading = this.store$.select(fromSuppliers.selectIsLoading);
-		this.sub.add(
-			this.store$.select(fromSuppliers.selectAllSuppliers).subscribe((suppliers: SupplierModel[]) => {
-				this.suppliers.data = suppliers;
-				this.suppliers.paginator = this.paginator;
-			})
-		);
-	}
+  ngOnInit(): void {
+    // Load all supplier
+    this.store$.dispatch(ClientActions.beginLoadingSuppliers());
+    this.isLoading = this.store$.select(fromSuppliers.selectIsLoading);
+    this.sub.add(
+        this.store$.select(fromSuppliers.selectAllSuppliers).subscribe((suppliers: SupplierModel[]) => {
+          this.suppliers.data = suppliers;
+          this.suppliers.paginator = this.paginator;
+        })
+    );
+  }
 
-	ngOnDestroy(): void {
-		this.sub.unsubscribe();
-	}
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
 
-	filterActions(data: SupplierModel, filterValue: string): boolean {
-		return data.email.toLowerCase().includes(filterValue)
-			|| data.notes.toLowerCase().includes(filterValue)
-			|| data.lastName.toLowerCase().includes(filterValue)
-			|| data.firstName.toLowerCase().includes(filterValue)
-			|| data.description.toLowerCase().includes(filterValue);
-	}
+  filterActions(data: SupplierModel, filterValue: string): boolean {
+    return data.email.toLowerCase().includes(filterValue)
+        || data.notes.toLowerCase().includes(filterValue)
+        || data.lastName.toLowerCase().includes(filterValue)
+        || data.firstName.toLowerCase().includes(filterValue)
+        || data.description.toLowerCase().includes(filterValue);
+  }
 
-	editSupplier(supplier: SupplierModel) {
-		this.store$.dispatch(SupplierActions.showEditSupplier({focusedSupplier: supplier}));
-	}
+  editSupplier(supplier: SupplierModel) {
+    this.store$.dispatch(SupplierActions.showEditSupplier({focusedSupplier: supplier}));
+  }
 
-	deleteSupplier(supplier: SupplierModel) {
-		this.store$.dispatch(SupplierActions.showDeleteSupplier({focusedSupplier: supplier}));
-	}
+  deleteSupplier(supplier: SupplierModel) {
+    this.store$.dispatch(SupplierActions.showDeleteSupplier({focusedSupplier: supplier}));
+  }
 }
