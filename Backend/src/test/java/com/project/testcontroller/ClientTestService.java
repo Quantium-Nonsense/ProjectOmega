013d6ed049dcaf2aafe9203db28d856e59a6490f
Kaylesh <56653205+Kaylesh@users.noreply.github.com>
2020-05-18 19:02:13 +0100

    test service client creation

--- Backend/src/test/java/com/project/testcontroller/ClientTestService.java
+++ Backend/src/test/java/com/project/testcontroller/ClientTestService.java
@@ -1,17 +1,24 @@
 package com.project.testcontroller;
 
 import com.project.OmegaApplicationTests;
+import com.project.omega.bean.dao.entity.Client;
+import com.project.omega.bean.dao.entity.Product;
 import com.project.omega.exceptions.ClientNotFoundException;
 import com.project.omega.exceptions.NoRecordsFoundException;
 import com.project.omega.repository.ClientRepository;
 import com.project.omega.service.implmentations.ClientServiceImpl;
 import com.project.omega.service.interfaces.ClientService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,13 +31,26 @@ public class ClientTestService extends OmegaApplicationTests {
 
     @Test
     public void createClientTest() throws NoRecordsFoundException {
+        Client client = new Client.ClientBuilder()
+                .setId(1L)
+                .setFirst_name("John")
+                .setLast_name("Doe")
+                .setDescription("Buys lots of things")
+                .setCompanyName("Lloyds")
+                .setEmail("a@a.com")
+                .setContactNumber("02088273729")
+                .setNotes("that's a random phone number")
+                .build();
 
+        Mockito.when(clientRepository.save(Mockito.any(Client.class))).thenReturn(client);
+        Mockito.when(clientRepository.findAll()).thenReturn(Stream.of(client).collect(Collectors.toList()));
+        Assert.assertEquals(1, clientService.getAllClients().size());
     }
 
 
     @Test
     public void getAllClientsTest_Positive() throws NoRecordsFoundException {
-
+        
     }
 
     @Test
@@ -55,7 +75,7 @@ public class ClientTestService extends OmegaApplicationTests {
 
     @Test
     public void deleteClientByIdTest() throws ClientNotFoundException {
-        
+
     }
 
 }
