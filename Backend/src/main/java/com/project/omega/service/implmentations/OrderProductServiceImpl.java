5008bdc6f9a2273dfe217519d1a282cc9fe59cb4
Xander9 <alex.karp475@gmail.com>
2020-05-19 10:38:09 +0100

    Verification token for confirming registration.

--- Backend/src/main/java/com/project/omega/service/implmentations/OrderProductServiceImpl.java
+++ Backend/src/main/java/com/project/omega/service/implmentations/OrderProductServiceImpl.java
@@ -6,9 +6,9 @@ import com.project.omega.service.interfaces.OrderProductService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-    @Service
-    @Transactional
-    public class OrderProductServiceImpl implements OrderProductService {
+@Service
+@Transactional
+public class OrderProductServiceImpl implements OrderProductService {
 
     private OrderProductRepository orderProductRepository;
 
