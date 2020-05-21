29b1043934c81da3740bd8005000be2ed2a8f895
dominikwasilewski <dominikwas@outlook.com>
2020-05-21 13:41:49 +0100

    master merge

--- AdminClient/src/app/chart/chart.component.ts
+++ AdminClient/src/app/chart/chart.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
-  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
-  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4']; // for each area of the doughnut chart
+  public doughnutChartData = [120, 150, 180, 90]; // amount for each area
   public doughnutChartType = 'doughnut';
   constructor() { }
   ngOnInit() {
