81a0b0a84e4be775465f5b4208ee97130fba5674
dominikwasilewski <dominikwas@outlook.com>
2020-05-22 15:26:59 +0100

    Merge remote-tracking branch 'origin/master' into adminCustomersToRepresentative
    # Conflicts:
    #	AdminClient/src/app/app.module.ts
    #	AdminClient/src/app/customers/customers.component.spec.ts
    #	AdminClient/src/app/customers/customers.component.ts
    #	AdminClient/src/app/customers/store/customers.actions.ts
    #	AdminClient/src/app/customers/store/customers.effects.ts
    #	AdminClient/src/app/customers/store/customers.reducer.ts
    #	AdminClient/src/app/models/orders/order.model.ts
    #	AdminClient/src/app/orders/order-details-dialog/order-details-dialog.component.html
    #	AdminClient/src/app/orders/order-details-dialog/order-details-dialog.component.ts
    #	AdminClient/src/app/orders/orders.component.html
    #	AdminClient/src/app/orders/orders.component.ts
    #	AdminClient/src/app/products/products.component.ts
    #	AdminClient/src/app/products/store/products.actions.ts
    #	AdminClient/src/app/products/store/products.effects.ts
    #	AdminClient/src/app/reducers/index.ts
    #	AdminClient/src/app/services/api-path.service.ts
    #	AdminClient/src/app/shared/shared.module.ts
    #	AdminClient/src/app/user/store/user.actions.ts
    #	AdminClient/src/app/user/store/user.effects.ts
    #	AdminClient/src/app/user/store/user.reducer.ts
    #	AdminClient/src/app/user/user.component.ts
    #	Backend/src/main/java/com/project/omega/controller/OrderController.java
    #	Backend/src/main/java/com/project/omega/service/implmentations/OrderServiceImpl.java
    #	Backend/src/main/java/com/project/omega/service/interfaces/OrderService.java

---
+++ AdminClient/src/app/shared/model/supplier/supplier.model.ts
@@ -0,0 +1,14 @@
+export class SupplierModel {
+	constructor(
+		public id: string,
+		public companyName: string,
+		public contactNumber: string,
+		public description: string,
+		public email: string,
+		public firstName: string,
+		public lastName: string,
+		public notes: string
+	) {
+	}
+
+}
---
+++ AdminClient/src/app/toolbar/toolbar.component.spec.ts
@@ -0,0 +1,92 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
+import { MatProgressBarHarness } from '@angular/material/progress-bar/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action, MemoizedSelector } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Observable } from 'rxjs';
+import { routes } from '../app-routing.module';
+import * as fromApp from '../reducers';
+import { TestModule } from '../shared/test/test.module';
+import * as fromToolbar from '../toolbar/store/toolbar.reducer';
+import { ToolbarEffects } from './store/toolbar.effects';
+import { ToolbarComponent } from './toolbar.component';
+
+describe('ToolbarComponent', () => {
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
+
+  let loader: HarnessLoader;
+  let documentLoader: HarnessLoader;
+
+  let mockStore: MockStore<fromApp.State>;
+  let mockProgressBarSelector: MemoizedSelector<fromToolbar.State, boolean>;
+  let effects: ToolbarEffects;
+  let actions$: Observable<Action>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ToolbarComponent],
+      providers: [
+        MatDialogHarness,
+        provideMockStore({
+          initialState: {
+            toolbar: {
+              progressBar: false,
+            }
+          } as fromApp.State
+        }),
+        provideMockActions(() => actions$),
+        ToolbarEffects,
+      ],
+      imports: [
+        TestModule,
+        RouterTestingModule.withRoutes(routes),
+      ]
+    })
+      .compileComponents();
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
+
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    documentLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
+
+    mockStore = TestBed.inject(MockStore);
+    effects = TestBed.inject<ToolbarEffects>(ToolbarEffects);
+
+    mockProgressBarSelector = mockStore.overrideSelector(
+      fromToolbar.selectIsProgressBarVisible,
+      false
+    );
+
+    mockStore.refreshState();
+    fixture.detectChanges();
+  }));
+
+  afterEach(async () => {
+    try {
+      const matDialog = await documentLoader.getHarness(MatDialogHarness);
+      if (matDialog) {
+        await matDialog.close();
+      }
+    } catch (e) {
+      // ignore means no dialog is up
+    }
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should present progress bar when loading anything', async () => {
+    component.ngOnInit();
+    mockProgressBarSelector.setResult(true);
+    mockStore.refreshState();
+    fixture.detectChanges();
+    const progressBar: MatProgressBarHarness = await loader.getHarness(MatProgressBarHarness);
+    expect(progressBar).toBeTruthy();
+  });
+});
