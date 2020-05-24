cb473364a308a21652a433d67d46512966becb3e
Xander9 <alex.karp475@gmail.com>
2020-05-24 13:28:29 +0100

    Stats apis for getting orders by rep and status.

--- Backend/src/main/java/com/project/omega/controller/StatsController.java
+++ Backend/src/main/java/com/project/omega/controller/StatsController.java
@@ -1,7 +1,9 @@
 package com.project.omega.controller;
 
 import com.project.omega.bean.dao.entity.OrderProduct;
+import com.project.omega.exceptions.UserNotFoundException;
 import com.project.omega.service.interfaces.OrderService;
+import com.project.omega.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,9 @@ import java.util.List;
 public class StatsController {
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping(value = "/top")
     public ResponseEntity topOrderedProducts() {
@@ -40,5 +45,41 @@ public class StatsController {
         });
 
         return new ResponseEntity(topProducts, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/orders/status")
+    public ResponseEntity ordersByStatus() {
+        HashMap<String, Integer> orders = new HashMap<>();
+
+        orderService.getAllOrders().forEach(order -> {
+            String status = order.getStatus();
+            if(orders.containsKey(status)) {
+                orders.replace(status, orders.get(status) + 1);
+            } else {
+                orders.put(status, 1);
+            }
+        });
+
+        return new ResponseEntity(orders, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/orders/rep")
+    public ResponseEntity ordersByRep() {
+        HashMap<String, Integer> orders = new HashMap<>();
+
+        orderService.getAllOrders().forEach(order -> {
+            try {
+                String rep = userService.getUserById(order.getUserId()).getEmail();
+                if(orders.containsKey(rep)) {
+                    orders.replace(rep, orders.get(rep) + 1);
+                } else {
+                    orders.put(rep, 1);
+                }
+            } catch (UserNotFoundException e) {
+                return;
+            }
+        });
+
+        return new ResponseEntity(orders, HttpStatus.OK);
     }
 }
