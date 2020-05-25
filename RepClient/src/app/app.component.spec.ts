5f3f8ac5a899426a2bac40c62120005c1c25d2a6
codefactor-io <support@codefactor.io>
2020-05-25 13:01:00 +0000

    [CodeFactor] Apply fixes to commit 29a9056

--- RepClient/src/app/app.component.spec.ts
+++ RepClient/src/app/app.component.spec.ts
@@ -29,7 +29,7 @@ describe('AppComponent', () => {
       ],
       imports: [
         RouterTestingModule.withRoutes([]),
-        LoggerTestingModule,
+        LoggerTestingModule
       ]
     }).compileComponents();
   }));
