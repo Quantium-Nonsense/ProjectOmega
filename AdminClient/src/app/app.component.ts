7c69d83ae0a999cf02e3eab551c568e11b380508
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-25 01:32:43 +0100

    change color for client 1

--- AdminClient/src/app/app.component.ts
+++ AdminClient/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import {Component} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   sideNavOpen = false;
   title = 'OmegaSys';
+
+  constructor(@Inject(DOCUMENT) private document) {
+  }
+
+  ngOnInit(): void {
+    console.log(window['env']['company']);
+    if (window['env']['company'] === 'client1') {
+      this.document.getElementById('theme').setAttribute('href', 'node_modules/@angular/material/prebuilt-themes/indigo-pink.css');
+    } else if (window['env']['company'] === 'client2') {
+      this.document.getElementById('theme').setAttribute('href', 'node_modules/@angular/material/prebuilt-themes/purple-green.css');
+    } else {
+      this.document.getElementById('theme').setAttribute('href', 'node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css');
+    }
+  }
 }
