b1f627e4fc1ae47028474566c834bebb078afc68
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-25 01:11:33 +0100

    Check new integration

--- AdminClient/src/app/app.component.ts
+++ AdminClient/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import {Component} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   sideNavOpen = false;
   title = 'OmegaSys';
+
+  constructor(@Inject(DOCUMENT) private document) {
+  }
+
+  ngOnInit(): void {
+    if (window['env']['company'] === 'client1') {
+      this.document.getElementById('theme').setAttribute('href', 'node_modules/@angular/material/prebuilt-themes/indigo-pink.css');
+    } else {
+      this.document.getElementById('theme').setAttribute('href', 'node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css');
+    }
+  }
 }
