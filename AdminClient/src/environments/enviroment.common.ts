a7cace9c9f0247269e51f65d0d04206939a1259d
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-25 12:39:09 +0100

    whitelist dynamic path

--- AdminClient/src/environments/enviroment.common.ts
+++ AdminClient/src/environments/enviroment.common.ts
@@ -5,7 +5,7 @@ export const commonEnvironment = {
   CANCELLATION_TEXT: 'Cancel',
   DELETE_CUSTOMER_CONFIRM_TEXT: 'Are you sure you want to delete this customer?',
   DELETE_CUSTOMER_TITLE: 'Delete Customer',
-  apiPath: '40.65.236.154',
+  apiPath: window['env']['apiRoot'] ,
   apiRoot: () => `http://${ window['env']['apiRoot'] }/api`,
   gitCompany: window['env']['company']
 };
